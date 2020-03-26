24af066a110d3e4b623b1465a98d0103ba508a2b
Grégory TAILLY <gregory.tailly@gmail.com>
2020-03-26 13:59:55 +0100

    :tada: first commit

---
+++ src/main/java/fr/gtailly/authentification/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package fr.gtailly.authentification.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
